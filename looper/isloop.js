b2885daffda4d6baba594fe120f6ae838c4f9beb
Dhalism <shneider.s@gmail.com>
2019-10-23 17:09:58 -0400

    'loops'

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,7 +2,48 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+  let dict = {}
+  let slow = linkedlist.head
+  let fast = linkedlist.head
+  let counter = 0
+  let dictvals = Object.values(dict)
+ do{
+    counter++
+    //console.log('dict ', dict)
+    if (slow in dictvals){
+      console.log('slow ', slow)
+    }
 
+    dict[counter] = slow
+    dictvals = Object.values(dict)
+
+
+    slow = slow.next
+    fast = fast.next.next
+
+    if (!fast.next || !fast.next.next || !slow.next){
+      return false
+    }
+  } while(fast !== slow)
+
+  return true
+  // let dict = { }
+  // dict[linkedlist.head.value] = linkedlist.head.value
+  // let checklist = linkedlist.head
+  // while(checklist.next){
+
+  //   if (!dict[checklist.next.value]){
+
+  //     dict[checklist.next.value] = checklist.next.value
+  //   }
+  //   else{
+
+  //     return  true
+  //   }
+
+  //   checklist = checklist.next
+  // }
+  // return false
 };
 
 
--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -3,6 +3,20 @@
 // Complete this algo
 const minJumps = arr => {
 
+  let start = 0
+  let end = arr[0]
+  let max;
+  let counter = 0
+  do{
+    console.log(arr)
+    end = arr[start]
+    console.log(start, end)
+    max = Math.max(...arr.slice(start, end))
+    console.log(max)
+    start = max;
+    counter++
+  }while(end<arr.length-1)
+  return counter;
 };
 
 module.exports = minJumps
