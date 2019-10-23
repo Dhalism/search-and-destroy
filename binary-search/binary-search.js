8327949e1fa7ae29e28b2ba1cd21b38be0d59ae7
Dhalism <shneider.s@gmail.com>
2019-10-23 15:43:19 -0400

    'binarysearch'

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,33 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+
+	let start = 0;
+	let end = array.length-1
+	let middle
+ while(array[middle]!==target)
+	{
+		middle = Math.ceil((end-start)/2)+start
+
+		if (target===array[middle]){
+			return true
+	 }
+		if (end<=start){
+			 return false
+		}
+
+		else if(target>array[middle]){
+
+			start = middle+1
+
+		}
+		else if(target<array[middle]){
+
+			end = middle-1
+		}
+	}
+
+	return false
 };
 
 /*
